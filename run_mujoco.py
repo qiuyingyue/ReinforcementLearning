d907b9ed6eca7a43bdddf161bb1daefa3760d078
Yingyue Qiu <qiuyingyue@Yingyues-MacBook-Pro.local>
2018-03-10 15:41:27 -0800

    framework

---
+++ run_mujoco.py
@@ -0,0 +1,58 @@
+
+
+import gym
+import sys
+sys.path.append("example")
+from DQN import DeepQNetwork
+def run_ant(rl_agent):
+    step = 0
+    for episode in range(300):
+        # initial observation
+        observation = env.reset()
+
+        while True:
+            # fresh env
+            env.render()
+            # RL choose action based on observation
+            action = rl_agent.choose_action(observation)
+
+            # RL take action and get next observation and reward
+            observation_, reward, done, info = env.step(action)
+
+            rl_agent.store_transition(observation, action, reward, observation_)
+
+            if (step > 200) and (step % 5 == 0):
+                rl_agent.learn()
+
+            # swap observation
+            observation = observation_
+
+            # break while loop when end of this episode
+            if done:
+                break
+            step += 1
+
+    # end 
+    print('over')
+    env.destroy()
+
+if __name__ == "__main__":
+    ###get environment
+    env = gym.make('Ant-v2')##HalfCheetah, Ant, Humanoid
+    #env = myEnv() #self-defined enviornment
+
+
+    ###initialize rl_agent
+    observation = env.reset()
+    action = env.action_space.sample()
+    rl_agent = DeepQNetwork(len(action), len(observation),
+                      learning_rate=0.01,
+                      reward_decay=0.9,
+                      e_greedy=0.9,
+                      replace_target_iter=200,
+                      memory_size=2000,
+                      # output_graph=True
+                      )
+    #parse rl_agent to run the environment
+    run_ant(rl_agent)
+    
